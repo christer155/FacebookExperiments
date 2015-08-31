90902a283c57347ff8f25f2f6b3924912d8b8790
HelainSchoonjans <helain90@hotmail.com>
2015-08-31 23:15:10 +0200

    Some error handling, and more tinkering.

--- project/main.py
+++ project/main.py
@@ -1,18 +1,42 @@
 # @author Hélain Schoonjans
 
-import facebook3 as fb 
+import facebook3 as fb
+import sys 
 # the content of this repository: https://github.com/tuanchauict/facebook-sdk-python3
 # that have been added to the site-packages folder of anaconda
 
 
+# add error message in case of missing file or bad token!
 oauth_access_token = open('../ressources/accessToken.txt').read()
 
 
 
 graph = fb.GraphAPI(oauth_access_token)
-profile = graph.get_object("me")
-friends = graph.get_connections("me", "friends")
 
-print(friends)
+friends = {}
+
+try:
+    some_friends = graph.get_connections("me", "friends")
+    print(some_friends)
+except:
+    print("Unexpected error:",  sys.exc_info()[0])
+
+
+#print(graph.fetch_url(some_friends['paging']['next']))
+
+#print(graph.get_connections("me", some_friends['data'][0]["id"]))
+
+
+
+# now i need a method to fill the friends dictionary
+    # get the sex
+    # get the likes
+
+
+# another to save the dictionary
+
+
+
+#profile = graph.get_object("me")
 #graph.put_object("me", "feed", message="I am writing on my wall!")
 
