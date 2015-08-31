b49e0b8d184d1a8d7cf7a577a1a77db6ed05196f
HelainSchoonjans <helain90@hotmail.com>
2015-08-31 21:31:19 +0200

    Project initialization

---
+++ setup.py
@@ -0,0 +1,23 @@
+import os
+from setuptools import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "experiments",
+    version = "0.0.1",
+    author = "Hélain Schoonjans",
+    author_email = "helain90@hotmail.com",
+    description = ("Some statistical experiments on Facebook."),
+    license = "MIT license",
+    keywords = "facebook statistics",
+    #url = "http://packages.python.org/an_example_pypi_project",
+    packages=['facebook-sdk'],
+    long_description=read('README'),
+    #classifiers=[
+    #    "Development Status :: 3 - Alpha",
+    #    "Topic :: Utilities",
+    #    "License :: OSI Approved :: BSD License",
+    #],
+) 
